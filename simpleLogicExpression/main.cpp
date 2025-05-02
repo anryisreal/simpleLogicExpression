0e078e5f1ac6ee297af8e4c5fd0ee6d31049e7b1
anryisreal <Krutt228zz@mail.ru>
2025-05-02 13:46:43 +0300

    Реализация заглушек к функциям программы

--- simpleLogicExpression/main.cpp
+++ simpleLogicExpression/main.cpp
@@ -17,3 +17,49 @@ int main() {
 	return 0;
 }
 
+
+std::string readFile(const std::string& filePath) {
+	std::string content = "";
+
+	return content;
+}
+
+void writeFile(const std::string& filePath, const std::string& content) {
+
+}
+
+std::vector<Token> tokenize(const std::string& expression, std::set<Error>& errorList) {
+	std::vector<Token> tokens;
+
+	return tokens;
+}
+
+ExpressionNode* buildExpressionTree(const std::vector<Token>& tokens, std::set<Error>& errorList) {
+	ExpressionNode* node = nullptr;
+
+	return node;
+}
+
+ExpressionNode* copyNode(ExpressionNode* node) {
+	ExpressionNode* newNode = nullptr;
+
+	return newNode;
+}
+
+void transformImplicationAndEquivalence(ExpressionNode* node) {
+
+}
+
+bool simplifyExpression(ExpressionNode* node) {
+	return false;
+}
+
+void removeDoubleNot(ExpressionNode* node) {
+
+}
+
+std::string expressionTreeToInfix(ExpressionNode* node) {
+	std::string result = "";
+
+	return result;
+}
